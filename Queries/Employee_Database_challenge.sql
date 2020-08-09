acf7f4cc98b5861b2245a67dfab7e3778889414f
Lubi Voroga <lubi@macbook-air.lan>
2020-08-09 13:45:18 -0400

    The Mentorship Eligibility table

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,6 +17,16 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
 --Create a retirement titles table
 SELECT e.emp_no,
     e.first_name,
@@ -47,3 +57,26 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY "count" DESC;
+
+-- Create the a new table
+SELECT * FROM retiring_titles;
+
+--Create Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+	de.from_date,
+    de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
